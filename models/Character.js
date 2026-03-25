eb5cd5fd237e3b37f3298fb5cb572bbe9471a177
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-03-25 12:33:50 +0100

    create the character and technique schema

---
+++ models/Character.js
@@ -0,0 +1,60 @@
+
+const mongoose = require('mongoose');
+
+const techniqueSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    manaCost: {
+        type:Number,
+        required: true
+    },
+    power: {
+        type: Number, 
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['attack', 'buff', 'debuff', 'defense', 'heal', 'combo'],
+        default: 'attack'
+    }
+})
+
+const characterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    stength: {
+        type: Number,
+        dafault: 100
+    },
+    stamina: {
+        type: Number,
+        default: 1000
+    },
+    defense: {
+        type: Number,
+        default: 50
+    },
+    speed: {
+        type: Number,
+        default: 10
+    },
+    mana: {
+        type: Number,
+        default: 100
+    },
+    technique: [techniqueSchema],
+    isActive: {
+        type: Boolean, 
+        default: true
+    }
+});
+
+module.exports = mongoose.model('Character', characterSchema);
+
