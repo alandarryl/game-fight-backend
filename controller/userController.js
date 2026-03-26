74d9ddf74bbec88dcd3b24c46897cb13579396e4
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-03-26 22:10:09 +0100

    add a few line in user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,15 @@
 
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+//Fonction pour générer le token JWT
+const generateToken = (id, role) =>{
+    return jwt.sign({id, role}, process.env.JWT_SECRET, {
+        expiresIn: '24h'
+    });
+};
+
+
+
 
 
