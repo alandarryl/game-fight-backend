09f0f1c449caf5d1641cc88d4084284fb74c59e9
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-03-27 22:44:30 +0100

    add the character router

---
+++ routes/characterRoutes.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const router = express.Router();
+
+const {
+    createCharacter,
+    updateCharacter,
+    deleteCharacter,
+    getCharacters,
+    toggleCharacterStatus
+} = require('../controller/characterController');
+
+const {protect, isAdmin} = require('../middleware/authMiddleware');
+
+//access by all user 
+
+router.get('/', protect, getCharacters);
+
+//only by admin
+router.post('/create', protect, isAdmin, createCharacter);
+router.put('/update/:id', protect, isAdmin, updateCharacter);
+router.patch('/:id/status', protect, isAdmin, toggleCharacterStatus);
+
+
+module.exports = router;
+
+
+
